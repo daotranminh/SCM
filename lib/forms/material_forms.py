6d8d7d77048929bfa3e41b9a6c3f49b9cb4979ae
daotranminh <daotranminh@gmail.com>
2019-09-07 15:28:39 +0200

    Adapt add_material form to new DB schema

--- lib/forms/material_forms.py
+++ lib/forms/material_forms.py
@@ -3,7 +3,10 @@ import logging
 from init import config
 from utilities import scm_constants
 
-from wtforms import Form, BooleanField, StringField, SelectField, DateTimeField, SubmitField, validators, widgets
+from wtforms import Form, BooleanField, StringField, SelectField, DateTimeField, SubmitField, validators
+
+from . import base_elements
+#from base_forms import MultiCheckboxField
 
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler(config['DEFAULT']['log_file'])
@@ -13,14 +16,11 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
 class AddMaterialForm(Form):
-    #name = StringField(_('Material name*'), [validators.InputRequired(), validators.Length(min=1, max=50)])
-    #unit = SelectField(_('Unit'))
-    #unit_price = StringField(_('Unit price*'), [validators.InputRequired(), validators.Regexp('^\d*[.,]?\d*$', message=_('Unit price must be a decimal number'))])
-    #is_organic = BooleanField(_('Is organic'))
     name = StringField('Material name*', [validators.InputRequired(), validators.Length(min=1, max=50)])
+    description = StringField('Description', [validators.Length(min=1, max=400)])
     unit = SelectField('Unit')
-    unit_price = StringField('Unit price*', [validators.InputRequired(), validators.Regexp('^\d*[.,]?\d*$', message='Unit price must be a decimal number')])
-    is_organic = BooleanField('Is organic')
+    #unit_price = StringField('Unit price*', [validators.InputRequired(), validators.Regexp('^\d*[.,]?\d*$', message='Unit price must be a decimal number')])
+    is_organic = base_elements.MultiCheckboxField('', coerce=int, choices=[(0, 'Is organic')]) 
 
     def __init__(self, form):
         super(AddMaterialForm, self).__init__(form)
